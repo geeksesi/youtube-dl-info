ffea36fe427ae714092e2e510e9bd1cd2157a005
JavadGH <geeksesi@gmail.com>
2019-02-02 20:43:53 +0330

    map changed...

--- youtubedl.js
+++ youtubedl.js
@@ -1,27 +1,42 @@
-class youtube_dl
-{
-    constructor ()
-    {
-        const { exec } = require('child_process');
-        this.exec = exec;
-    }
-    info (link)
-    {
-        link = decodeURI(link);
-        link = "youtube-dl '"+ link +"' -s -J";
-        let obj_return ={};
-        this.exec(link, (error, stdout, stderr) =>{
-            if (typeof JSON.parse(stdout) != null)
-            {
-                console.log(stdout);
+const {exec} = require('child_process');
+let fs = require('fs');
+
+function youtube_info(link) {
+    let youtube_array = {};
+    link = decodeURI(link);
+    link = "youtube-dl '" + link + "' -s -J";
+    exec(link, {maxBuffer: 1000 * 4096}, (error, stdout, stderr) => {
+
+        if (error !== null) {
+            return false;
+        }
+        let data;
+        try {
+            data = JSON.parse(stdout);
+        } catch (e) {
+            return false;
+        }
+        if (typeof data["_type"] != null && data["_type"] === "playlist") {
+            for (let episods of data.entries) {
+                let parts = {
+                    title: episods.title,
+                    thumbnail: episods.thumbnail,
+                    description: episods.description,
+                    formats: []
+                };
+                for (let format of data.entries.formats) {
+                    let form = {
+                        id: format.format_id,
+                        size: format.filesize,
+                        type: format.ext,
+                        url: format.url,
+                        desc: format.format
+                    };
+                }
+                console.log(episods.id);
             }
-            else
-            {
-                console.log(error);
-            }
-        });
-        return obj_return;
-    }
+        }
+    });
 }
 
-module.exports = youtube_dl;
+module.exports = youtube_info;
